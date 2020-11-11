4c405fd7ffa015affe06cae3d657d4fff5e2df52
Jan Janßen <janssen@mpie.de>
2020-11-11 22:16:15 +0100

    Add csv table

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p ${PREFIX}/share/iprpy
+python generate_csv.py
+cp "potentials_lammps.csv" ${PREFIX}/share/iprpy 
 cp -r potential_LAMMPS ${PREFIX}/share/iprpy
 cp -r Potential ${PREFIX}/share/iprpy
 cp -r Citation ${PREFIX}/share/iprpy
