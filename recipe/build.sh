940eb155318f19b293a45dde73f7f2b3b35f6403
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-11-11 22:29:20 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ${PREFIX}/share/iprpy
-python generate_csv.py
+python ${RECIPE_DIR}/generate_csv.py
 cp "potentials_lammps.csv" ${PREFIX}/share/iprpy 
 cp -r potential_LAMMPS ${PREFIX}/share/iprpy
 cp -r Potential ${PREFIX}/share/iprpy
